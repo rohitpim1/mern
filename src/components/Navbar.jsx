40ed7f9261d2af862b86da0c87e205be022df713
rohitpim1 <pimrohit33@gamil.com>
2023-06-05 17:56:21 +0530

    PRP-6 added new dependency, created mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Logo from './Logo'
 import './navbar.css'
+import {FaBars,FaTimes} from 'react-icons/fa'
 import NavButton from './NavButton'
 
 const Navbar = () =>{
+    const[menuToggle,setMenuToggle] = useState(false)
+    const mobileList =(
+        <ul className={`mobileList ${menuToggle ? "active" : null}`} onClick={()=>setMenuToggle(false)}>
+            <li >Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    )
     return(
+        
         <nav className='navbar wrapper'>
          <ul className='navList'>
           <li>Home</li>
@@ -12,9 +22,19 @@ const Navbar = () =>{
           <li>Contact</li>
          </ul>
          <Logo/>
-         <NavButton/>
+       <div className="navbtn">
+       <NavButton className="navbtn"/>
+       </div>
+         <div className='menu-icon' onClick={()=>setMenuToggle(!menuToggle)}>
+            {!menuToggle ? <FaBars style={{fontSize:"22px"}}/> : <FaTimes style={{fontSize:"22px"}}/>}
+         </div>
+         {
+            menuToggle && mobileList
+         }
         </nav>
+        
+       
     )
-}
+}   
 
 export default Navbar;
